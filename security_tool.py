3deaeb5ff013c04b9b4bc48c2ef4b9f9b98f95cd
kadour tan <mariakouchkar5@gmail.com>
2025-02-24 14:33:48 +0100

    working on new things

---
+++ security_tool.py
@@ -0,0 +1,17 @@
+import hashlib
+
+def hash_password(password):
+    
+    return hashlib.sha256(password.encode()).hexdigest()
+
+def main():
+    password = input("Enter your password: ")
+    hashed_password = hash_password(password)
+    
+
+    
+    hidden_passphrase = "cGFzc3Rhbg"  
+    print("Security tool is running...")
+
+if __name__ == "__main__":
+    main()
